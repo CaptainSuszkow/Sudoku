671c222b865a36cd414f20fcb10fc40de7689e0b
216895 Suszek Michał <216895@edu.p.lodz.pl>
2019-05-08 10:29:50 +0200

    Changed Dao Test

--- Model/src/test/java/FileSudokuBoardDaoTest.java
+++ Model/src/test/java/FileSudokuBoardDaoTest.java
@@ -3,15 +3,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileSudokuBoardDaoTest {
-    SudokuBoard board = new SudokuBoard();
-    SudokuSolver solver = new BacktrackingSudokuSolver();
+
 
     @Test
-    void read() {
-        FileSudokuBoardDao reader = new FileSudokuBoardDao("file.txt");
+    void readAndWrtie() {
+        SudokuBoard board = new SudokuBoard();
+        SudokuSolver solver = new BacktrackingSudokuSolver();
+        FileSudokuBoardDao steam = new FileSudokuBoardDao("file.txt");
 
-        board = reader.read();
+        solver.solve(board);
 
+        steam.write(board);
+
+        SudokuBoard board1 = steam.read();
+
+        assertEquals(board.hashCode(),board1.hashCode());
+        
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                System.out.print(board.get(i,j)+" ");
@@ -21,12 +28,5 @@ class FileSudokuBoardDaoTest {
 
     }
 
-    @Test
-    void write() {
-        FileSudokuBoardDao writer = new FileSudokuBoardDao("file.txt");
 
-        solver.solve(board);
-
-        writer.write(board);
-    }
 }
